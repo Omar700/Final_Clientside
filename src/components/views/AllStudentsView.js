4a86cdfcf6233e2004b6d0b9ca9127fb5865c2d4
Omar <omar.arafa51@myhunter.cuny.edu>
2025-05-13 03:44:55 -0400

    all students complete

--- src/components/views/AllStudentsView.js
+++ src/components/views/AllStudentsView.js
@@ -7,45 +7,61 @@ It constructs a React component to display the all students view page.
 import { Link } from "react-router-dom";
 
 const AllStudentsView = (props) => {
-  const {students, deleteStudent} = props;
+  const { students, deleteStudent } = props;
+
   // If there is no student, display a message
   if (!students.length) {
     return (
-    <div>
-      <p>There are no students.</p>
-      <Link to={`newstudent`}>
-        <button>Add New Student</button>
-      </Link>
-    </div>
+      <div>
+        <p>There are no students.</p>
+        <Link to={`/newstudent`}>
+          <button>Add New Student</button>
+        </Link>
+      </div>
     );
   }
-  
+
   // If there is at least one student, render All Students view 
   return (
     <div>
       <h1>All Students</h1>
 
       {students.map((student) => {
-          let name = student.firstname + " " + student.lastname;
-          return (
-            <div key={student.id}>
-              <Link to={`/student/${student.id}`}>
-                <h2>{name}</h2>
-              </Link>
-              <button onClick={() => deleteStudent(student.id)}>Delete</button>
-              <hr/>
-            </div>
-          );
-        }
-      )}
-      <br/>
+        let name = student.firstName + " " + student.lastName;
+        return (
+          <div key={student.id} style={{ marginBottom: '20px' }}>
+            <img
+              src={student.imageUrl || "https://placehold.co/150x150"}
+              alt={`${name}'s profile`}
+              width="150"
+              height="150"
+            />
+            <h2>
+              <Link to={`/student/${student.id}`}>{name}</Link>
+            </h2>
+            <p><strong>Email:</strong> {student.email}</p>
+            <p><strong>GPA:</strong> {student.gpa ?? "N/A"}</p>
+            <p>
+              <strong>Campus:</strong>{" "}
+              {student.campus ? (
+                <Link to={`/campus/${student.campus.id}`}>{student.campus.name}</Link>
+              ) : (
+                "Not enrolled"
+              )}
+            </p>
+            <button onClick={() => deleteStudent(student.id)}>Delete</button>
+            <hr />
+          </div>
+        );
+      })}
+
+      <br />
       <Link to={`/newstudent`}>
         <button>Add New Student</button>
       </Link>
-      <br/><br/>
+      <br /><br />
     </div>
   );
 };
-
 
 export default AllStudentsView;
